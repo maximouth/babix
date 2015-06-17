461cab8f47712bb8d1bb98e0496a0fc38a0be127
maximouth <maxime@localhost.localdomain>
2015-06-17 16:37:37 +0200

    Correction de qq encarts incorrects en terme d'opérande I vs O.

--- mutex.cpp
+++ mutex.cpp
@@ -10,43 +10,34 @@ int Debug = 1;
 
 
 //essayer de prendre le mutex, fini quand on l'obtient
-void takeM (int *mutex) {
-
-
-  //valeur du mutex pris
-  asm volatile ("mov r1, #1  \n\t");
-  asm volatile ("mov r2, #0  \n\t");
-
-  asm volatile ("take:  \n\t");
+void takeM (int *mutex)
+{
+  asm volatile (".take:  \n\t");
   //lire la valeur du mutex
-  asm volatile ("ldrex r5, [%0]   \n\t" : "+r" (mutex));
+  asm volatile ("ldrex r5, [%0]   \n\t" :: "r" (mutex));
 
-  /*// debug
-  asm volatile ("mov %0, r5 \n\t" : "+r" (Debug));
-  Serial.print("D ");
-  Serial.println(Debug);
-  //*/
+  /* Debug
+     asm volatile ("mov %0, r5 \n\t" : "+r" (Debug));
+     Serial.print("D ");
+     Serial.println(Debug);
+  */
 
   //regarder sa valeur
   // si prise(==1) recommencer la fonction
   asm volatile ("mov r2, #1  \n\t");
   asm volatile ("cmp r2, r5  \n\t");
-  asm volatile ("beq take  \n\t");
+  asm volatile ("beq .take  \n\t");
   
   //sinon essayer de prendre le verrou
-  //valeur retour dans R0 tester avec la valeur prise dans R1, a l'adresse du mutex
-  asm volatile ("mov r1, #1  \n\t");
-  asm volatile ("strex r5, r1, [%0]  \n\t" : "+r" (mutex));
-  
+  //valeur retour dans R0 tester avec la valeur prise dans R1, a l'adresse du
+  //mutex
+  asm volatile ("strex r5, r2, [%0]  \n\t" :: "r" (mutex) : "memory");
 
   //regarder si la valeur de retour est bonne ou pas
   // 0 reussite 1 fail
   // Si non retourner au debut
-  asm volatile ("mov r8, #1  \n\t");
-  asm volatile ("cmp r8, r5  \n\t");
-  asm volatile ("beq take  \n\t");
-  // Si oui on à pris le verrou et on fait ce qu'on à a faire
-  //Serial.println("b");  
+  asm volatile ("cmp r2, r5  \n\t");
+  asm volatile ("beq .take  \n\t");
   return;
 
 }
@@ -55,7 +46,7 @@ void takeM (int *mutex) {
 // libere le mutex (à appeller à la fin de la section critique)
 void freeM(int *mutex) {
   asm volatile ("mov r2, #0  \n\t");
-  asm volatile ("str r2, [%0]  \n\t" : "+r" (mutex) );
+  asm volatile ("str r2, [%0]  \n\t" :: "r" (mutex) : "memory");
   return;
 }
 
